7d03b3e2ccf2a5fc3b2c07eb2e9f0bd506c9c834
Pravin Kumar <115208590+Pravinkumar295@users.noreply.github.com>
2025-05-15 23:07:25 +0530

    Add files via upload

---
+++ chatapplication/src/main/java/com/chatapp/Testingapp1Application.java
@@ -0,0 +1,13 @@
+package com.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testingapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testingapp1Application.class, args);
+	}
+
+}
